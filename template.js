1699ff3a144ed91917144279a5989d3f79af3144
Alartan <alartatrala@interia.pl>
2021-05-18 15:43:17 +0200

    O nas

--- template.js
+++ template.js
@@ -4,11 +4,12 @@ window.onload = () => {
     <header>
       <nav>
         <ul>
-          <li><a href="index.html"><i class="material-icons" style="font-size:18px;color:red;
-            font-variant: initial;">home</i></a></li>
+          <li><a href="index.html"><i class="material-icons" style="font-size:16px;color:red;
+            font-variant: normal;">home</i></a></li>
           <li><a href="roboty.html">Roboty</a></li>
           <li><a href="https://www.roblox.com/games/4654198932/Plansze-Robotyka-ZSP8?refPageId=e8793651-de4a-40d9-b5d2-1e5faca99b8d">Roblox</a></li>
           <li><a href="artworks.html">Artworks</a></li>
+          <li><a href="o_nas.html">O nas</a></li>
         </ul>
       </nav>
     </header>`;
